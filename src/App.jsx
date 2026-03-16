d4d93d7cda047ac2a129fc7d78fc0b5bf0e93ca7
kkimna0101 <kkimna0101@gmail.com>
2026-03-16 14:38:46 +0900

    Move Header/Footer to common and update App imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 import Main from './pages/Main';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
@@ -16,33 +16,33 @@ import Board from './pages/Board';
 import NotFound from './pages/NotFound';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/product" element={<ProductList />} />
-          <Route path="/women" element={<ProductList />} />
-          <Route path="/men" element={<ProductList />} />
-          <Route path="/kids" element={<ProductList />} />
-          <Route path="/sale" element={<ProductList />} />
-          <Route path="/best" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/complete" element={<Complete />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/find-account" element={<FindAccount />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/board/:type" element={<Board />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="main">
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/product" element={<ProductList />} />
+                    <Route path="/women" element={<ProductList />} />
+                    <Route path="/men" element={<ProductList />} />
+                    <Route path="/kids" element={<ProductList />} />
+                    <Route path="/sale" element={<ProductList />} />
+                    <Route path="/best" element={<ProductList />} />
+                    <Route path="/product/:id" element={<ProductDetail />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/complete" element={<Complete />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/find-account" element={<FindAccount />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/board/:type" element={<Board />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
